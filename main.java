59e25d0a908b56674277d59197d9be338c1ee257
Máyra <mayrazucaro@gmail.com>
2024-01-15 10:58:20 -0300

    feat initial logic

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(setLetters("works"));
+	}
+	
+	public static ArrayList<Character> setLetters(String word) {
+	    ArrayList<Character> letters = new ArrayList<>();
+    	IntStream.range(0,word.length())
+    			.forEach(i -> letters.add(word.charAt(i)));
+    	return letters;		
+    }
+}
