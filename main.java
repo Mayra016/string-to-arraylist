631e2962ddf5c1c473cfcab9ec78af55562d6d97
Máyra <mayrazucaro@gmail.com>
2024-01-15 12:56:53 -0300

    feat add arraylist to word function

--- main.java
+++ main.java
@@ -4,13 +4,22 @@ import java.util.stream.IntStream;
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println(setLetters("works"));
+	     System.out.println(setLetters("works"));
+	     System.out.println(arrayToString(setLetters("works")));
 	}
 	
 	public static ArrayList<Character> setLetters(String word) {
 	    ArrayList<Character> letters = new ArrayList<>();
     	    IntStream.range(0,word.length())
-    			.forEach(i -> letters.add(word.charAt(i)));
-    	return letters;		
-    }
+    		.forEach(i -> letters.add(word.charAt(i)));
+    	    return letters;	
+	}
+	
+	public static String arrayToString(ArrayList<Character> letters) {
+            StringBuilder word = new StringBuilder();
+            IntStream.range(0, letters.size())
+                .forEach(i -> word.append(letters.get(i)));
+            return word.toString();    
+        }	
+    
 }
